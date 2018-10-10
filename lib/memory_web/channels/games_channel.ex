aa0f94995f4be507772216616a9b4a6a6c8dc915
devanship <devanship96@gmail.com>
2018-10-10 15:41:30 -0400

    don't push

--- lib/memory_web/channels/games_channel.ex
+++ lib/memory_web/channels/games_channel.ex
@@ -9,18 +9,18 @@ defmodule MemoryWeb.GamesChannel do
     {:ok, %{"join" => game, "game" => view}, socket}
   end
 
-  # def handle_in("click", %{"i" => i}, socket) do
-  #   view = GameServer.guess(socket.assigns[:game], socket.assigns[:user], i)
-  #   # if(game.cardsFlipped == 2) do
-  #   #   {:reply, {:unflip, %{ "game" => view }}, socket}
-  #   # else
-  #   #   {:reply, {:ok, %{ "game" => view }}, socket}
-  #   # end
-  #   {:reply, {:ok, %{ "game" => view }}, socket}
-  # end
+  def handle_in("click", %{"i" => i}, socket) do
+    view = GameServer.unflip(socket.assigns[:game], socket.assigns[:user], i)
+    # if(game.cardsFlipped == 2) do
+    #   {:reply, {:unflip, %{ "game" => view }}, socket}
+    # else
+    #   {:reply, {:ok, %{ "game" => view }}, socket}
+    # end
+    {:reply, {:ok, %{ "game" => view }}, socket}
+  end
 
   def handle_in("unflip", payload, socket) do
-    Memory.GameBackup.save(socket.assigns[:name], socket.assigns[:game])
+    # Memory.GameBackup.save(socket.assigns[:name], socket.assigns[:game])
     view = GameServer.unflip(socket.assigns[:game], socket.assigns[:user])
     {:reply, {:ok, %{ "game" => view }}, socket}
   end
