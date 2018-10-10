1eb1758d2c4ee1684672d7ce3b1d8c512b24b77e
sarahchou <chou.s@husky.neu.edu>
2018-10-10 15:39:58 -0400

    added handle_in click method and add_user to join

--- lib/memory_web/channels/games_channel.ex
+++ lib/memory_web/channels/games_channel.ex
@@ -5,6 +5,7 @@ defmodule MemoryWeb.GamesChannel do
 
   def join("games:" <> game, payload, socket) do
     socket = assign(socket, :game, game)
+    GameServer.add_user(game, socket.assigns[:user])
     view = GameServer.view(game, socket.assigns[:user])
     {:ok, %{"join" => game, "game" => view}, socket}
   end
@@ -18,11 +19,18 @@ defmodule MemoryWeb.GamesChannel do
   #   # end
   #   {:reply, {:ok, %{ "game" => view }}, socket}
   # end
-
-  def handle_in("unflip", payload, socket) do
-    Memory.GameBackup.save(socket.assigns[:name], socket.assigns[:game])
+  
+  # TODO fix this handle_in
+  def handle_in("click", payload, socket) do
     view = GameServer.unflip(socket.assigns[:game], socket.assigns[:user])
     {:reply, {:ok, %{ "game" => view }}, socket}
+  end
+
+  def handle_in("unflip", payload, socket) do
+    #Memory.GameBackup.save(socket.assigns[:name], socket.assigns[:game])
+    view = GameServer.unflip(socket.assigns[:game], socket.assigns[:user])
+    {:reply, {:ok, %{ "game" => view }}, socket}
+
   end
 
   def handle_in("restart", payload, socket) do
