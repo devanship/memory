b721b31226913ed61e1575a0d52c5411d3693f7a
sarahchou <chou.s@husky.neu.edu>
2018-10-10 18:10:44 -0400

    Added unflip and click handle

--- lib/memory_web/channels/games_channel.ex
+++ lib/memory_web/channels/games_channel.ex
@@ -5,27 +5,21 @@ defmodule MemoryWeb.GamesChannel do
 
   def join("games:" <> game, payload, socket) do
     socket = assign(socket, :game, game)
-    GameServer.add_user(:game, socket.assigns[:user])
+    GameServer.add_user(game, socket.assigns[:user])
     view = GameServer.view(game, socket.assigns[:user])
     {:ok, %{"join" => game, "game" => view}, socket}
   end
 
   def handle_in("click", %{"i" => i}, socket) do
-    game = Game.new()
-    view = GameServer.unflip(socket.assigns[:game], socket.assigns[:user])
-    if(game.cardsFlipped == 2) do
+    view = GameServer.click(socket.assigns[:game], socket.assigns[:user], i)
+    player = socket.assigns[:user]
+    if(player.cardsFlipped == 2) do
       {:reply, {:unflip, %{ "game" => view }}, socket}
     else
       {:reply, {:ok, %{ "game" => view }}, socket}
     end
     {:reply, {:ok, %{ "game" => view }}, socket}
   end
-
-  # TODO fix this handle_in
-  # def handle_in("click", payload, socket) do
-  #   view = GameServer.unflip(socket.assigns[:game], socket.assigns[:user])
-  #   {:reply, {:ok, %{ "game" => view }}, socket}
-  # end
 
   def handle_in("unflip", payload, socket) do
     #Memory.GameBackup.save(socket.assigns[:name], socket.assigns[:game])
